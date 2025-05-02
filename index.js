90adc5aaf7a075df0910a0241a5f8eabb8f00131
Daniel Andrade Silva <danielandradesilva.84@gmail.com>
2025-05-02 09:03:07 -0300

    Altera Cors e adiciona get na rota root.

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
 //config json response
-app.use(express.json())
+app.use(express.json());
 
 //solve cors: origin deve receber a url do front
-app.use(cors({ credentials: true, origin: 'http://localhost:5000'}))
+app.use(cors());
 
 //public folder for images
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //routes
-const UserRoutes = require('./routes/UserRoutes')
-app.use('/users', UserRoutes)
+app.get("/", (req, res) => {
+  res.send("<h1>🚀 Servidor rodando com sucesso!</h1>");
+});
 
-const EmployeeRoutes = require('./routes/EmployeeRoutes')
-app.use('/employees', EmployeeRoutes)
+const UserRoutes = require("./routes/UserRoutes");
+app.use("/users", UserRoutes);
 
-const EmployeeHealthExaminationRoutes = require('./routes/EmployeeHealthExaminationRoutes')
-app.use('/employee-health-examinations', EmployeeHealthExaminationRoutes)
+const EmployeeRoutes = require("./routes/EmployeeRoutes");
+app.use("/employees", EmployeeRoutes);
 
-const ActivityRoutes = require('./routes/ActivityRoutes')
-app.use('/activities', ActivityRoutes)
+const EmployeeHealthExaminationRoutes = require("./routes/EmployeeHealthExaminationRoutes");
+app.use("/employee-health-examinations", EmployeeHealthExaminationRoutes);
 
-const ActivityRequiredTrainingRoutes = require('./routes/ActivityRequiredTrainingRoutes')
-app.use('/activity-required-trainings', ActivityRequiredTrainingRoutes)
+const ActivityRoutes = require("./routes/ActivityRoutes");
+app.use("/activities", ActivityRoutes);
 
-const HealthExaminationRoutes = require('./routes/HealthExaminationRoutes')
-app.use('/health-examinations', HealthExaminationRoutes)
+const ActivityRequiredTrainingRoutes = require("./routes/ActivityRequiredTrainingRoutes");
+app.use("/activity-required-trainings", ActivityRequiredTrainingRoutes);
 
-const TaskRoutes = require('./routes/TaskRoutes')
-app.use('/tasks', TaskRoutes)
+const HealthExaminationRoutes = require("./routes/HealthExaminationRoutes");
+app.use("/health-examinations", HealthExaminationRoutes);
 
-const TrainingReceivedRoutes = require('./routes/TrainingReceivedRoutes')
-app.use('/training-received', TrainingReceivedRoutes)
+const TaskRoutes = require("./routes/TaskRoutes");
+app.use("/tasks", TaskRoutes);
 
-const TrainingRoutes = require('./routes/TrainingRoutes')
-app.use('/trainings', TrainingRoutes)
+const TrainingReceivedRoutes = require("./routes/TrainingReceivedRoutes");
+app.use("/training-received", TrainingReceivedRoutes);
 
+const TrainingRoutes = require("./routes/TrainingRoutes");
+app.use("/trainings", TrainingRoutes);
 
-app.listen(5000)
+app.listen(5000);
